6102b1055d30af166ae42217249bf93a51bdd60c
Aleksi Markkanen <muffa@riemann.kyla.fi>
2012-02-23 20:20:41 +0200

    foo

--- src/gol/Main.java
+++ src/gol/Main.java
@@ -16,6 +16,10 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+             GoLUI cntPn = new GoLUI();
+             
+             cntPn.main(new String[1]);
+             
     }
 
 }
